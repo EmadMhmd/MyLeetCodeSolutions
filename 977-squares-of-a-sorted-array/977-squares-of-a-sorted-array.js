bf1ad7663b3b9ceb9d1b59b7a925fa94e83758c0
Emad Mohamed <48203154+EmadMhmd@users.noreply.github.com>
2022-05-25 16:00:08 +0200

    Time: 156 ms (38.57%), Space: 48.5 MB (80.98%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -4,10 +4,10 @@
  */
 var sortedSquares = function(nums) {
     let absNums = [];
+    
     for(let i=0;i<nums.length;i++){
         absNums.push(Math.pow(Math.abs(nums[i]),2));
     }
     
     return absNums.sort(function(a, b){return a-b});
-
 };
